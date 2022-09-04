ebd85d2f27391bbf761033fdeeae15fddce2daea
xaMMax <petrolijuice@gmail.com>
2022-09-04 13:32:37 +0200

    add JS function for foto

---
+++ script/main.js
@@ -0,0 +1,8 @@
+function foto_onclick() {
+    document.getElementById('foto').style.display = 'unset'
+    document.getElementById('foto-btn').style.display = 'none'
+}
+function button() {
+    document.getElementById('foto-btn').style.display='unset'
+    document.getElementById('foto').style.display = 'none'
+}
