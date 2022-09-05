7d7fef08236e5d1d9de9c7d780a43e5fe13b4b06
xaMMax <petrolijuice@gmail.com>
2022-09-05 22:27:26 +0200

    add more changes into css then into html and js

--- script/main.js
+++ script/main.js
@@ -1,8 +1,10 @@
 function foto_onclick() {
-    document.getElementById('foto').style.display = 'unset'
-    document.getElementById('foto-btn').style.display = 'none'
+  document.getElementById("foto").style.display = "unset";
+  document.getElementById("foto-btn").style.display = "none";
+  document.getElementById("info-container").style.margin = "6% 0 0 0";
 }
 function button() {
-    document.getElementById('foto-btn').style.display='unset'
-    document.getElementById('foto').style.display = 'none'
+  document.getElementById("foto-btn").style.display = "unset";
+  document.getElementById("foto").style.display = "none";
+  document.getElementById("info-container").style.margin = "6% 0 6% 0";
 }
