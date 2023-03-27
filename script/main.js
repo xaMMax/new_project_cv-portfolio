fa4b8649b77664084e19189e88a350e0c027525b
Maks Kaydash <36572876+xaMMax@users.noreply.github.com>
2023-03-27 10:45:19 +0200

    Comment all JS

--- script/main.js
+++ script/main.js
@@ -1,10 +1,10 @@
-function foto_onclick() {
-  document.getElementById("foto").style.display = "run-in";
-  document.getElementById("foto-btn").style.display = "none";
-  document.getElementById("info-container").style.margin = "6% 0 0 0";
-}
-function button() {
-  document.getElementById("foto-btn").style.display = "none";
-  document.getElementById("foto").style.display = "unset";
-  document.getElementById("info-container").style.margin = "6% 0 6% 0";
-}
+// function foto_onclick() {
+//   document.getElementById("foto").style.display = "run-in";
+//   document.getElementById("foto-btn").style.display = "none";
+//   document.getElementById("info-container").style.margin = "6% 0 0 0";
+// }
+// function button() {
+//   document.getElementById("foto-btn").style.display = "none";
+//   document.getElementById("foto").style.display = "unset";
+//   document.getElementById("info-container").style.margin = "6% 0 6% 0";
+// }
